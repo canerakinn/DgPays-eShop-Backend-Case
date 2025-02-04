77ef013fce2bf8843831e339ea28c8c42d2db922
Caner Akın <cnr.akn@hotmail.com>
2025-02-05 02:56:33 +0300

    add project

---
+++ src/Ordering.API/Application/DomainEventHandlers/OrderCompletedDomainEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using Ordering.Domain.Events;
+
+namespace eShop.Ordering.API.Application.DomainEventHandlers
+{
+    public class OrderCompletedDomainEventHandler : INotificationHandler<OrderStatusChangedToCompletedDomainEvent>
+    {
+        private readonly IOrderingIntegrationEventService _orderingIntegrationEventService;
+        private  readonly ILogger<OrderCompletedDomainEventHandler> _logger;
+
+        public OrderCompletedDomainEventHandler(
+        IOrderingIntegrationEventService orderingIntegrationEventService,
+        ILogger<OrderCompletedDomainEventHandler> logger)
+        {
+            _orderingIntegrationEventService = orderingIntegrationEventService;
+            _logger = logger;
+        }
+
+        public async Task Handle(OrderStatusChangedToCompletedDomainEvent domainEvent,
+        CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Order {OrderId} completed, creating integration event",
+                domainEvent.OrderId);
+            var orderCompletedIntegrationEvent = new OrderCompletedIntegrationEvent(domainEvent.OrderId);
+            await _orderingIntegrationEventService.AddAndSaveEventAsync(orderCompletedIntegrationEvent);
+        }
+    }
+}
---
+++ src/Ordering.API/Application/IntegrationEvents/Events/OrderCompletedIntegrationEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace eShop.Ordering.API.Application.IntegrationEvents.Events
+{
+    public record OrderCompletedIntegrationEvent : IntegrationEvent
+    {
+        // Using record type for built-in value equality and immutability
+        public int OrderId { get; }
+
+        public OrderCompletedIntegrationEvent(int orderId)
+        {
+            OrderId = orderId;
+        }
+    }
+}
